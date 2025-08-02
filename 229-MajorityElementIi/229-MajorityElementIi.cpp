47498c55753a8532f1d0f00d784303ccc3123c28
Ekta3006 <57767742+Ekta3006@users.noreply.github.com>
2025-08-02 17:11:52 +0530

    Create 229-MajorityElementIi

---
+++ 229-MajorityElementIi/229-MajorityElementIi.cpp
@@ -0,0 +1,82 @@
+// Last updated: 8/2/2025, 5:11:52 PM
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& a) {
+        int element1=a[0];
+   int element2=0;
+
+   int count1=1;
+
+   int count2=0;
+
+   int n=a.size();
+
+   for(int i=1;i<n;i++){
+
+       if(a[i]==element1){
+
+           count1++;
+
+       }
+
+       else if(a[i]==element2){
+
+           count2++;
+
+       }
+
+       else if(count1==0){
+
+           element1=a[i];
+
+           count1=1;
+
+       }
+
+       else if(count2==0){
+
+           element2=a[i];
+
+           count2=1;
+
+       }
+
+       else{
+
+           count1--;
+
+           count2--;
+
+       }
+
+   }  
+
+       vector<int> v;
+
+       int ct1=0,ct2=0;
+
+       for(int i=0;i<n;i++){
+
+           if(a[i]==element1)
+
+               ct1++;
+
+           else if(a[i]==element2)
+
+               ct2++;
+
+       }
+
+       if(ct1>floor(n/3))
+
+           v.push_back(element1);
+
+       if(ct2>floor(n/3))
+
+           v.push_back(element2);
+
+       sort(v.begin(),v.end());
+
+       return v;
+    }
+};
