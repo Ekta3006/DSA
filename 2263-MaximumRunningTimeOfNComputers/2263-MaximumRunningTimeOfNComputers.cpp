810810b2d559fcb5055f6daee10f58549bb51e2b
Ekta3006 <57767742+Ekta3006@users.noreply.github.com>
2025-08-02 17:10:43 +0530

    Create 2263-MaximumRunningTimeOfNComputers

---
+++ 2263-MaximumRunningTimeOfNComputers/2263-MaximumRunningTimeOfNComputers.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/2/2025, 5:10:42 PM
+class Solution {
+public:
+  long long maxRunTime(int n, std::vector<int>& batteries) {
+        std::sort(batteries.begin(), batteries.end());
+        long long left = 1, right = std::accumulate(batteries.begin(), batteries.end(), 0LL) / n;
+        while (left < right) {
+            long long target = right - (right - left) / 2;
+            long long total = std::accumulate(batteries.begin(), batteries.end(), 0LL, [&target](long long a, int b) { return a + std::min(static_cast<long long>(b), target); });
+            if (total >= target * n) {
+                left = target;
+            } else {
+                right = target - 1;
+            }
+        }
+      return left;
+        }
+};
