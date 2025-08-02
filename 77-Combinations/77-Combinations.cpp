e088fe714cdee669a8c111e4f2722a67b0b5dc9a
Ekta3006 <57767742+Ekta3006@users.noreply.github.com>
2025-08-02 17:12:27 +0530

    Create 77-Combinations

---
+++ 77-Combinations/77-Combinations.cpp
@@ -0,0 +1,22 @@
+// Last updated: 8/2/2025, 5:12:27 PM
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        vector<int> combination(k);
+        generateCombinations(1, n, k, combination, result);
+        return result;
+    }
+
+private:
+    void generateCombinations(int start, int n, int k, vector<int> &combination, vector<vector<int>> &result) {
+        if (k == 0) {
+            result.push_back(combination);
+            return;
+        }
+        for (int i = start; i <= n; ++i) {
+            combination[combination.size() - k] = i;
+            generateCombinations(i + 1, n, k - 1, combination, result);
+        }
+    }
+};
