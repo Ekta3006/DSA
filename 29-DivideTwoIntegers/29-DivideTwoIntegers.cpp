110b6490fe4329065a3dd105ab85f82e9ad75f98
Ekta3006 <57767742+Ekta3006@users.noreply.github.com>
2025-08-02 17:12:47 +0530

    Create 29-DivideTwoIntegers

---
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.cpp
@@ -0,0 +1,46 @@
+// Last updated: 8/2/2025, 5:12:47 PM
+class Solution {
+public:
+    int divide(int a, int b) {
+        if (b == 1) {
+            return a;
+        }
+        if (a == INT_MIN && b == -1) {
+            return INT_MAX;
+        }
+
+        bool sign;
+        if ((a > 0 && b > 0) || (a < 0 && b < 0)) {
+            sign = true;
+        } else {
+            sign = false;
+        }
+
+        
+        if (a > 0) {
+            a = -a;
+        }
+        if (b > 0) {
+            b = -b;
+        }
+
+        int ans = 0;
+        while (a <= b) {
+            int x = b;
+            int cnt = 1;
+            while (x >= (INT_MIN >> 1) && a <= (x << 1)) {
+                x <<= 1;
+                cnt <<= 1;
+            }
+            ans += cnt;
+            a -= x;
+        }
+
+        
+        if (sign) {
+            return ans;
+        } else {
+            return -ans;
+        }
+    }
+};
