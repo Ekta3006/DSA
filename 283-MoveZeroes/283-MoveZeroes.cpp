f132cfc0da1021ca1a109781d0c5ad4cd14104b4
Ekta3006 <57767742+Ekta3006@users.noreply.github.com>
2025-08-02 17:11:46 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/2/2025, 5:11:45 PM
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int lastNonZeroFoundAt = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroFoundAt++] = nums[i];
+            }
+        }
+
+        for (int i = lastNonZeroFoundAt; i < n; i++) {
+            nums[i] = 0;
+        }
+    }
+};
