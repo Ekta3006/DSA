e7d539416dab86189a23b2e20a58af9f7cda17c2
Ekta3006 <57767742+Ekta3006@users.noreply.github.com>
2025-08-02 17:12:58 +0530

    Create 17-LetterCombinationsOfAPhoneNumber

---
+++ 17-LetterCombinationsOfAPhoneNumber/17-LetterCombinationsOfAPhoneNumber.cpp
@@ -0,0 +1,27 @@
+// Last updated: 8/2/2025, 5:12:57 PM
+class Solution {
+public:
+   vector<string> ans;
+    vector<string> dial = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    
+    vector<string> letterCombinations(string digits) {
+        ans.clear();
+        if (digits.length() == 0) {
+            return ans;
+        }
+        helper("", 0, digits);
+        return ans;
+    }
+    
+    void helper(string comb, int index, string digits) {
+        if (index == digits.length()) {
+            ans.push_back(comb);
+            return;
+        }
+            
+        string letters = dial[digits[index] - '0'];
+        for (int i = 0; i < letters.length(); i++) {
+            helper(comb + letters[i], index + 1, digits);
+        }
+    }
+};
