535aa67de08876d000c4c407fc306a9a052a4d00
Ekta3006 <57767742+Ekta3006@users.noreply.github.com>
2025-08-02 17:11:43 +0530

    Create 349-IntersectionOfTwoArrays

---
+++ 349-IntersectionOfTwoArrays/349-IntersectionOfTwoArrays.cpp
@@ -0,0 +1,20 @@
+// Last updated: 8/2/2025, 5:11:43 PM
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+           bool s[1001];
+        memset(s, false, sizeof(s));
+        for (int x : nums1) {
+            s[x] = true;
+        }
+        vector<int> ans;
+        for (int x : nums2) {
+            if (s[x]) {
+                ans.push_back(x);
+                s[x] = false;
+            }
+        }
+        return ans;
+    }
+};
+    
